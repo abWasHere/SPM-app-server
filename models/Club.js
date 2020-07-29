e8c3dad87423d0b1a9c6b52fd8d3a69ded6bd5ff
Aude Richon <auderichon@gmail.com>
2020-07-29 18:47:47 +0200

    club model

---
+++ models/Club.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const clubSchema = new Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  clubName: { type: String, required: true },
+  image: { type: String, default: URL("../public/images/happy-bunch.png") },
+  address: {
+    type: { type: String, enum: ["Point"] },
+    coordinates: { type: [Number] },
+    formattedAddress: String,
+  },
+  phoneNumber: String,
+  website: String,
+  videoURL: String,
+  year: String,
+  subscriptionFee: String,
+  description: String,
+  role: { type: String, enum: ["Club"], default: "Club" },
+  favoritePlayers: [
+    {
+      player: {
+        type: Schema.Types.ObjectId,
+        ref: "Player",
+      },
+    },
+  ],
+  favoriteClubs: [
+    {
+      club: {
+        type: Schema.Types.ObjectId,
+        ref: "Club",
+      },
+    },
+  ],
+});
+
+const Club = mongoose.model("Club", clubSchema);
+
+module.exports = Club;
