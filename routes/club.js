a6ef28b8326333f100b50d24d8073de32a352fe3
Aude Richon <auderichon@gmail.com>
2020-07-30 16:00:25 +0200

    update routes club avec get all

--- routes/club.js
+++ routes/club.js
@@ -7,47 +7,59 @@ const uploader = require("./../config/cloudinary");
 // ROUTES PREFIX IS    /api/club
 //  --------------------------------------
 
+// GET ALL CLUBS
+
+router.get("/", (req, res, next) => {
+  clubModel
+    .findById(req.params.id)
+    .select(-password)
+    .then((dbRes) => {
+      res.status(200).json(dbRes);
+    })
+    .catch(next);
+});
+
 // CLUB GET INFOS
 
 router.get("/:id", (req, res, next) => {
-	clubModel
-		.findById(req.params.id)
-		.select(-password)
-		.populate("Player Club")
-		.then((dbRes) => {
-			res.status(200).json(dbRes);
-		})
-		.catch(next);
+  clubModel
+    .findById(req.params.id)
+    .select(-password)
+    .populate("Player Club")
+    .then((dbRes) => {
+      res.status(200).json(dbRes);
+    })
+    .catch(next);
 });
 
 // CLUB EDIT ACCOUNT
 
 router.patch("/edit/:id", uploader.single("image"), (req, res, next) => {
-	const updatedInfos = req.body;
+  const updatedInfos = req.body;
 
-	if (req.file) updatedInfos.image = req.file.path;
+  if (req.file) updatedInfos.image = req.file.path;
 
-	clubModel
-		.findByIdAndUpdate(req.params.id, updatedInfos, { new: true })
-		.select(-password)
-		.then((updatedUser) => {
-			res.status(200).json(updatedUser);
-		})
-		.catch(next);
+  clubModel
+    .findByIdAndUpdate(req.params.id, updatedInfos, { new: true })
+    .select(-password)
+    .then((updatedUser) => {
+      res.status(200).json(updatedUser);
+    })
+    .catch(next);
 });
 
 // CLUB DELETE ACCOUNT
 // TODO: display confirmation pop up on the front end before getting to this route
 
 router.post("/delete/:id", (req, res, next) => {
-	clubModel
-		.findByIdAndRemove(req.params.id)
-		.then(() => {
-			res.sendStatus(204);
-		})
-		.catch((err) => {
-			res.sendStatus(500);
-		});
+  clubModel
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
