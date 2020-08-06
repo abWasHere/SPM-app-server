7db3292df2ecc0c97a9f2089e7860c6b1a86c897
Aude Richon <auderichon@gmail.com>
2020-08-06 23:50:03 +0200

    validation messages and update photoc club

--- routes/player.js
+++ routes/player.js
@@ -53,38 +53,37 @@ router.get("/:id", (req, res) => {
 // PLAYER EDIT ACCOUNT
 
 router.patch(
-	"/:id",
-	protectPrivateRoute,
-	uploader.single("picture"),
-	(req, res) => {
-		const updatedInfos = req.body;
+  "/:id",
+  protectPrivateRoute,
+  uploader.single("picture"),
+  (req, res) => {
+    const updatedInfos = req.body;
 
-		if (req.file) updatedInfos.picture = req.file.path;
+    if (req.file) updatedInfos.picture = req.file.path;
 
-		playerModel
-			.findByIdAndUpdate(req.params.id, updatedInfos, { new: true })
-			.select("-password")
-			.then((user) => {
-				req.session.currentUser = updatedUser;
-				res.status(200).json(user);
-			})
-			.catch((err) => {
-				res.status(500).json(err);
-			});
-	}
+    playerModel
+      .findByIdAndUpdate(req.params.id, updatedInfos, { new: true })
+      .then((user) => {
+        req.session.currentUser = updatedUser;
+        res.status(200).json(user);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  }
 );
 
 // PLAYER DELETE ACCOUNT
 
 router.delete("/:id", protectPrivateRoute, (req, res) => {
-	playerModel
-		.findByIdAndRemove(req.params.id)
-		.then(() => {
-			res.sendStatus(204);
-		})
-		.catch((err) => {
-			res.sendStatus(500);
-		});
+  playerModel
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
